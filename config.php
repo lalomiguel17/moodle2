95f0e97c47a72ea73ab6b577e51d6df7dc7b97be
lalomiguel17 <L16091178@zacatepec.tecnm.mx>
2021-01-30 17:24:20 -0600

    xZxz
    cambio a la direccion moodle1

--- config.php
+++ config.php
@@ -132,7 +132,7 @@ $CFG->dboptions = array(
 // If you need both intranet and Internet access please read
 // http://docs.moodle.org/en/masquerading
 
-$CFG->wwwroot   = 'http://localhost/moodle';
+$CFG->wwwroot   = 'http://localhost/moodle1';
 
 
 //=========================================================================
