21e52855145669fce522a5bd260903c97805bf00
lalomiguel17 <L16091178@zacatepec.tecnm.mx>
2021-01-30 16:37:13 -0600

    cambio para mi carpeta en moodle
    dsadsa

--- config.php
+++ config.php
@@ -132,7 +132,7 @@ $CFG->dboptions = array(
 // If you need both intranet and Internet access please read
 // http://docs.moodle.org/en/masquerading
 
-$CFG->wwwroot   = 'http://192.168.1.106/moodle';
+$CFG->wwwroot   = 'http://localhost/moodle1';
 
 
 //=========================================================================
